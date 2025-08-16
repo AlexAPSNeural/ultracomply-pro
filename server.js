6ba8d9123f42a27b3fcaa29175555e961f507793
AlexAPSNeural <alex@apsneural.com>
2025-08-16 00:22:25 -0700

    Add server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Welcome to UltraComply Pro');
+});
+
+// Compliance routes
+app.use('/compliance', require('./routes/compliance'));
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
